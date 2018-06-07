db8d83373057bc7697ea0d3b75abb81df245ca0f
Robbie Page <rob@rorpage.com>
2018-06-06 23:18:59 -0400

    initial commit
    Signed-off-by: Robbie Page <rob@rorpage.com>

---
+++ FunctionHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input)
+        {
+            int value = DateTime.Now.Year;
+            if (!string.IsNullOrWhiteSpace(input))
+            {
+                dynamic jsonResponse = JsonConvert.DeserializeObject(input);
+                if (jsonResponse.value != null)
+                {
+                    value = (int) jsonResponse.value;
+                }
+            }
+
+            var output = new { factors = GetFactors(value) };
+            Console.WriteLine(JsonConvert.SerializeObject(output));
+        }
+
+        private int[] GetFactors(int value)
+        {
+            return Enumerable.Range(1, value).Where(i => value % i == 0).ToArray();
+        }
+    }
+}
